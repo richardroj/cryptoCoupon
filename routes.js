9b803503623b36452a6b89c050d808d8d87561c7
Richard Rojas <richardroj@gmail.com>
2018-08-28 22:31:21 +0100

    routes

--- routes.js
+++ routes.js
@@ -4,6 +4,8 @@ routes
   .add('/cryptoCoupons/new', '/cryptoCoupons/new')
   .add('/cryptoCoupons/advancedcoupon/new', '/cryptoCoupons/advancedcoupon/new')
   .add('/cryptoCoupons/:address', '/cryptoCoupons/show')
+  .add('/cryptoCoupons/:address/coupons', '/cryptoCoupons/coupons/index')
+  .add('/cryptoCoupons/:address/coupons/new', '/cryptoCoupons/coupons/new');
 
 
 module.exports = routes;
